c13179e6d81cd9634e9d6cb4728f05314f3c21e7
amro22397 <amroalmutasim22@gmail.com>
2025-01-31 09:37:23 +0400

    2auth empty

--- app/api/2auth/[...nextauth]/route.ts
+++ app/api/2auth/[...nextauth]/route.ts
@@ -1,52 +1,59 @@
-import mongoose from "mongoose";
-import { NextAuthOptions } from "next-auth";
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google"
-import CredentialsProvider from "next-auth/providers/credentials"
-import { User } from "../../../../models/User";
+// import mongoose from "mongoose";
+// import { NextAuthOptions } from "next-auth";
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google"
+// import CredentialsProvider from "next-auth/providers/credentials"
+// import { User } from "../../../../models/User";
+
+import { NextRequest, NextResponse } from "next/server";
 
 
 
-export const authOptions: NextAuthOptions = {
-    secret: process.env.NEXTAUTH_SECRET,
+// export const authOptions: NextAuthOptions = {
+//     secret: process.env.NEXTAUTH_SECRET,
     
-    providers: [
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                email: {
-                    label: "Email",
-                    type: "email",
-                    placeholder: "example@example.com"
-                },
-                password: { label: "Password", type: "password" },
-            },
+//     providers: [
+//         CredentialsProvider({
+//             name: 'Credentials',
+//             credentials: {
+//                 email: {
+//                     label: "Email",
+//                     type: "email",
+//                     placeholder: "example@example.com"
+//                 },
+//                 password: { label: "Password", type: "password" },
+//             },
 
-            async authorize(credentials) {
-                if (!credentials || !credentials.email || !credentials.password)
-                    return null;
+//             async authorize(credentials) {
+//                 if (!credentials || !credentials.email || !credentials.password)
+//                     return null;
 
-                mongoose.connect(process.env.MONGO_URL as string);
-                const dbUser = await User.findOne({email: credentials.email});
-
-
-                if (dbUser && dbUser.password === credentials.password) {
-                    const { password, createdAt, id, ...dbUserWithoutPassword } = dbUser;
-                    return dbUserWithoutPassword as any;
-                }
-
-                return null;
-            },
-        }),
+//                 mongoose.connect(process.env.MONGO_URL as string);
+//                 const dbUser = await User.findOne({email: credentials.email});
 
 
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        })
-    ],
+//                 if (dbUser && dbUser.password === credentials.password) {
+//                     const { password, createdAt, id, ...dbUserWithoutPassword } = dbUser;
+//                     return dbUserWithoutPassword as any;
+//                 }
+
+//                 return null;
+//             },
+//         }),
+
+
+//         GoogleProvider({
+//             clientId: process.env.GOOGLE_CLIENT_ID as string,
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//         })
+//     ],
+// }
+
+// const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST }
+
+
+export default async function handler(req: NextRequest, res: NextResponse) {
+
 }
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST }

e1d15e7bfaf73c45d744183291d7e759efa95b06
amro22397 <amroalmutasim22@gmail.com>
2025-02-05 11:39:56 +0400

    fix move right empty

--- app/api/projects/move-right/route.js
+++ app/api/projects/move-right/route.js
@@ -1,21 +1,26 @@
-import mongoose from "mongoose";
-import { Project } from "../../../../models/project";
+// import mongoose from "mongoose";
+// import { Project } from "../../../../models/project";
+
+// export async function PUT(req) {
+//     mongoose.connect(process.env.MONGO_URL)
+//     const url = new URL(req.url);
+//     const id = url.searchParams.get('id');
+
+//     const project = await Project.updateOne({_id: id},
+//         { $inc: { x: 1, y: -1 } },
+//       { new: true }
+//      )
+
+//      if (!project) {
+//         console.log('item not found');
+//         return null;
+//      }
+
+//      return Response.json(project);
+
+// }
+
 
 export async function PUT(req) {
-    mongoose.connect(process.env.MONGO_URL)
-    const url = new URL(req.url);
-    const id = url.searchParams.get('id');
-
-    const project = await Project.updateOne({_id: id},
-        { $inc: { x: 1, y: -1 } },
-      { new: true }
-     )
-
-     if (!project) {
-        console.log('item not found');
-        return null;
-     }
-
-     return Response.json(project);
 
 }

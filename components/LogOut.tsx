dcf14a02a2dfb7fc1c5fb13c10a7a7149743d980
amro22397 <amroalmutasim22@gmail.com>
2025-02-06 06:59:12 +0400

    maybe final edit

--- components/LogOut.tsx
+++ components/LogOut.tsx
@@ -2,6 +2,7 @@
 
 import { signOut } from "next-auth/react";
 import { useLocale, useTranslations } from "next-intl";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const LogOut = () => {
@@ -9,10 +10,26 @@ const LogOut = () => {
     const locale = useLocale();
     const header = useTranslations("Header");
 
+    const pathname = usePathname();
+    console.log(pathname)
+
+    console.log(pathname?.split(locale))
+
+    let afterLocale = pathname?.split(locale)[1];
+
+    console.log(pathname?.split('/'))
+
+    console.log(afterLocale?.includes(locale));
+
+    if (afterLocale?.includes(locale)) {
+      afterLocale = `/${pathname?.split('/')[2]}`
+      console.log(afterLocale);
+    }
+
   return (
     <button
       onClick={() => {
-        signOut({ callbackUrl: `/${locale}/` });
+        signOut({ callbackUrl: `/${locale}${afterLocale}` });
       }}
       className="text-red-600 hover:underline
               dark:text-red-300 
